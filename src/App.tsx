a6132599b4583ed24d438ec05e72475c1d83431e
chaim <a548492309@gmail.com>
2025-04-27 00:01:59 +0300

    add a8

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
-import { useState } from "react";
-import {
-  BasicCalendar,
-  ControlCalendar,
-  CustomizingCalendar,
-  AdvancedCalendar,
-} from "./Components";
-import CalenterWtoutALib from "./Components/CalenterWtoutALib";
-import Game from "./Components/Game/Game";
-import { Canvas } from "@react-three/fiber";
-import ReactThreeFiber from "./Components/react-three-fiber/ReactThreeFiber";
-import ChipComp from "./Components/Chip/Chip";
-import CheckboxesTags from "./Components/CheckboxesTags/CheckboxesTags";
-import CheckboxesTags2 from "./Components/CheckboxesTags/CheckboxesTags2";
-// import AIFace from "./Components/AIFace/AIFace";
-// import AIFace2 from "./Components/AIFace/AIFace2";
-import Bee from "./Components/Bee/Bee";
-import ChaimCymermanWebsite from "./Components/ChaimCymermanWebsite/ChaimCymermanWebsite";
+
 import Calendar from "./Components/Calendar";
 
 function App() {
   return (
     <div style={{ height: "95vh", width: "100%" }}>
-      {/* <BasicCalendar /> */}
-      {/* <CalenterWtoutALib/> */}
-      {/* <ControlCalendar /> */}
-      {/* <CustomizingCalendar /> */}
-      {/* <AdvancedCalendar /> */}
-      {/* <Game/> */}
-      {/* <ReactThreeFiber/> */}
-      {/* <ChipComp/> */}
-      {/* <CheckboxesTags/> */}
-      {/* <CheckboxesTags2></CheckboxesTags2> */}
-      {/* <AIFace/> */}
-      {/* <Bee/> */}
+   
       <Calendar/>
-      {/* <AIFace2 /> */}
-      {/* <ChaimCymermanWebsite /> */}
+     
     </div>
   );
 }
